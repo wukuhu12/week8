aa49f92547383b362413a90fbb83244cc28c19a8
wukuhu12 <dsfd93812@gmail.com>
2022-09-20 02:20:47 +0000

    week8

--- LogRocket/20220615-random/hardhat.config.js
+++ LogRocket/20220615-random/hardhat.config.js
@@ -19,25 +19,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 /**
- * @type import('hardhat/config').HardhatUserConfig
+ * 
  */
 
 module.exports = {
   solidity: "0.8.4",
-/*
   networks: {
-    "local-devnode": {
-       url: "http://localhost:8545",
-       accounts: { mnemonic: "test test test test test test test test test test test junk" }
-    },
-    "optimistic-kovan": {
-       url: "https://kovan.optimism.io",
-       accounts: { mnemonic: process.env.MNEMONIC }
-    },
+    
     "optimism": {
-       url: "https://mainnet.optimism.io",
-       accounts: { mnemonic: process.env.MNEMONIC }
+       url: "https://opt-goerli.g.alchemy.com/v2/8CEdJFDsDJnqtKtRp0PAp3HQsYXL0jX_",
+       accounts: [ "fbc743cd28d212dac4ac5d6f49af85d36bc94c088e151358cbafa94a48991840" ]
     }
   }
-*/
 };
